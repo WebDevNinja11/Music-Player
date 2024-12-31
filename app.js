8585338b832ab1e23233e707e8e329f166c1ae73
The Syob <michnalex3@gmail.com>
2024-12-30 20:33:05 -0500

    update?

--- app.js
+++ app.js
@@ -179,6 +179,7 @@ function appendArtistsToMenu() {
         menu.appendChild(dropdown);
     });
 }
-
+const playbtn = document.getElementById("play-pause-button");
+playbtn.onclick = togglePlayPause();
 // Initialize the page
 appendArtistsToMenu();
